61a2440aab6477a359b025eb0f8a0964e22a60f3
Anderson L. Sergio <anderson.als04@gmail.com>
2020-12-21 16:38:04 -0300

    Ensure that the DB connection is closed

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -27,6 +27,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
 	repository := repositories.NewUsersRepository(db)
 	userID, err := repository.Create(user)
